20f8f47ef58f70bf338383eff9025df04a93aa4d
Dheeraj Sharma <dheeraj9410096874@gmail.com>
2025-03-12 12:17:19 +0530

    UC10 implemented.

---
+++ HelloGreetingApplication/RepositoryLayer/Entity/UserEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.Entity
+{
+    public class UserEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; } // Hashed Password after salting
+    }
+}
---
+++ HelloGreetingApplication/RepositoryLayer/Service/UserRL.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Extensions.Configuration;
+using ModelLayer.Model;
+using RepositoryLayer.Context;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Hashing;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.Service
+{
+    public class UserRL : IUserRL
+    { 
+        private readonly HelloGreetingContext _dbContext;
+        private readonly Password_Hash _passwordHash;
+
+        public UserRL(HelloGreetingContext context, IConfiguration config, Password_Hash hash)
+        {
+            _dbContext = context;
+            _passwordHash = hash;
+        }
+
+
+        public UserEntity RegisterUser(RegisterModel userRegistrationDto)
+        {
+            var existingUser = _dbContext.Users.FirstOrDefault(e => e.Email == userRegistrationDto.Email);
+
+            if (existingUser == null)
+            {
+                var newUser = new UserEntity
+                {
+                    FirstName = userRegistrationDto.FirstName,
+                    LastName = userRegistrationDto.LastName,
+                    Email = userRegistrationDto.Email,
+                    PasswordHash = _passwordHash.PasswordHashing(userRegistrationDto.Password)
+                };
+
+                _dbContext.Users.Add(newUser);
+                _dbContext.SaveChanges();
+
+                return newUser;
+            }
+            return null;
+        }
+
+        public UserModel LoginUser(LoginModel userLoginDto)
+        {
+            var validUser = _dbContext.Users.FirstOrDefault(e => e.Email == userLoginDto.Email);
+            if (validUser != null)
+            {
+                if (_passwordHash.VerifyPassword(userLoginDto.Password, validUser.PasswordHash))
+                {
+                    return new UserModel()
+                    {
+                        FirstName = validUser.FirstName,
+                        LastName = validUser.LastName,
+                        Email = validUser.Email
+                    };
+                }
+
+            }
+            return null;
+        }
+
+        public Task<string> ForgetPassword(string email)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool ResetPassword(string newPassword, int userId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
